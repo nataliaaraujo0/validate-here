fc91c8f1c3b5d038ab5fdd5b7c3e52f6d51ce5f6
alucard <devjrteste@gmail.com>
2021-10-29 15:32:20 -0300

    revert: aplicando switch case

--- js/scripts.js
+++ js/scripts.js
@@ -11,22 +11,46 @@ function validaForm() {
   var wrapperName = document.getElementById("form-field-name");
   var wrapperEmail = document.getElementById("form-field-email");
 
-  if (nameStr.length > 0 && nameStr.length >= 3) {
-    wrapperName.querySelector("span").classList.add("success");
-    wrapperName.querySelector("span").innerText = "Successfully validated!";
-    fieldName.style.display = "none";
-    fieldEmail.style.display = "none";
-    validateButton.style.display = "none";
-    titleWelcome.innerText = "ok, all right!";
-  } else {
-    wrapperName.querySelector("span").classList.add("error");
-    wrapperName.querySelector("span").innerText = "Invalid information.";
+  switch (nameStr.length) {
+    case 0:
+      wrapperName.querySelector("span").classList.add("error");
+      wrapperName.querySelector("span").innerText = "Empty field";
+
+      break;
+
+    case 1:
+      wrapperName.querySelector("span").classList.add("error");
+      wrapperName.querySelector("span").innerText =
+        "You need to enter at least 3 characters";
+
+      break;
+
+    case 2:
+      wrapperName.querySelector("span").classList.add("error");
+      wrapperName.querySelector("span").innerText =
+        "You need to enter at least 3 characters";
+
+      break;
+
+    case 3:
+      wrapperName.querySelector("span").classList.add("success");
+      wrapperName.querySelector("span").innerText =
+        "Validated! more than a short name?!";
+
+      break;
+
+    default:
+      wrapperName.querySelector("span").classList.add("success");
+
+      fieldName.style.display = "none";
+      fieldEmail.style.display = "none";
+      validateButton.style.display = "none";
+      titleWelcome.innerText = "ok, all right!";
   }
 
   if (isEmail.test(emailStr)) {
     wrapperEmail.querySelector("span").classList.add("success");
-    wrapperEmail.querySelector("span").innerText =
-      "Email validado com sucesso!";
+    wrapperEmail.querySelector("span").innerText = "successfully validated!";
   } else {
     wrapperEmail.querySelector("span").classList.add("error");
     wrapperEmail.querySelector("span").innerText = "Invalid email";
